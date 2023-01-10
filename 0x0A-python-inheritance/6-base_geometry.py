1f013f6f47eb0a13113d10c24f6ce6e7abc230a1
Festongithub <fkemboik@gmail.com>
2023-01-10 00:56:39 -0800

    Done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""BaseGeometry
+
+"""
+class BaseGeometry:
+    """Contains Function for area()"""
+
+    def area(self):
+        """Function not implemented yet
+        """
+
+        raise Exception("area() is not implemented")
