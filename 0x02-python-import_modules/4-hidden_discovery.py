ed3a1482205cb0907d66c90a81e8df1e48a3a8f4
Festongithub <fkemboik@gmail.com>
2024-01-18 02:26:26 -0800

    Python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 
 if __name__ == "__main__":
     import sys
-    import hidden_4 as  hidden
+    import hidden_4 as hidden
     for name in dir(hidden):
         if name[0:2] != "__":
             print(name)
