8db198d3dbd2901c8c949c11357e70cc28de2a9e
Festongithub <fkemboik@gmail.com>
2024-01-18 01:30:28 -0800

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+import sys
 
-if __name__ == "__main__":
-    from sys import argv
-    x = len(argv) - 1
-    if x < 1:
-        print("{} arguments".format(x))
-    elif x == 1:
-        print("{} arguments".format(x))
-    else:
-        print("{} arguments".format(x))
-        for i in range(x):
-            print("{}:{}".format(i+1, argv[i+1]))
+if __name__ != "__main__":
+    exit()
+
+argStr = "{:d} argument"
+argc = len(sys.argv) - 1
+if argc == 0:
+    argStr += 's.'
+elif argc == 1:
+    argStr += ':'
+else:
+    argStr += 's:'
+print(argStr.format(argc))
+
+i = 0
+for arg in sys.argv:
+    if i != 0:
+        print("{:d}: {:s}".format(i, arg))
+    i += 1
