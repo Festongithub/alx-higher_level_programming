c446a6fcd0b5c623b038f95d74faf0493095df83
Festongithub <fkemboik@gmail.com>
2024-01-18 01:37:01 -0800

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3 
 
 if __name__ == "__main__":
-    from hidden_4 import *
-    allf = dir()
-    for i in range(0, len(allf)):
-        if [i][:2] != "_":
-            print("{:s}".format(allf[i]))
+    import sys
+    import hidden_4 as  hidden
+    for name in dir(hidden):
+        if name[0:2] != "__":
+            print(name)
