cd630766001dd5994bb8fd086e54998e8f7ae047
Festongithub <fkemboik@gmail.com>
2024-01-24 03:19:11 -0800

    Lists anatomy

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
-            'C': 100, 'D': 500, 'M': 1000}
-    result = 0
-    prev_value = 0
-    for char in roman_string[::-1]:
-        value = roman_dict[char]
-        if value < prev_value:
-            result -= value
-        else:
-            result += value
-        prev_value = value
-    return result
+    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    p = 0
+
+    if type(roman_string) is str and roman_string:
+        for c in range(len(roman_string) - 1, -1, -1):
+            if val[roman_string[c]] >= p:
+                res += val[roman_string[c]]
+            else:
+                res -= val[roman_string[c]]
+            p = val[roman_string[c]]
+    return res
