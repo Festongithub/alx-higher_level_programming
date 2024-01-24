7e27aaa8832c6049d27f773a48f70d99dc222898
Festongithub <fkemboik@gmail.com>
2024-01-24 02:49:43 -0800

    Likes lists

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,4 +3,4 @@ def complex_delete(a_dictionary, value):
     for k, v in list(a_dictionary.items()):
         if v is value:
             a_dictionary.pop(k)
-        return a_dictionary
+    return a_dictionary
