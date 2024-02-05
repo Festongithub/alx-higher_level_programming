2451f3387b227c481bf6954c83b21845a54bfe9d
Festongithub <fkemboik@gmail.com>
2024-02-05 03:50:13 -0800

    Lion

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,10 +4,11 @@ from __future__ import print_function
 
 import sys
 
+
 def safe_function(fct, *args):
     try:
         res = fct(*args)
-    except Exception as  e:
+    except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
     else:
