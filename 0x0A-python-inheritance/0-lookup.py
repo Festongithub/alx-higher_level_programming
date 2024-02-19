c343cc172b9d6a57dfea2fb9b4322a1da7bcf60b
Festongithub <fkemboik@gmail.com>
2024-02-19 07:00:27 -0800

    who am I

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+returns the list of available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    return methods on object
+    """
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+Inheritance in Python, is an important aspect in OOP, this is a real representation of the actual objects
