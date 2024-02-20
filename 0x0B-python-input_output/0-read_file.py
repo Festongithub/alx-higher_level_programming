2a80ff4d5e86aae10d926205960505aa112a9a52
Festongithub <fkemboik@gmail.com>
2024-02-20 01:54:27 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,5 @@ Writes a file in stdup
 
 
 def read_file(filename=""):
-    if filename:
-        with open(filename, mode="r", encoding="utf-8") as f:
-            for line in f:
-                print(line, end="")
+    with open(filename,"r", encoding="UTF-8") as f:
+        print(f.read(), end="")
