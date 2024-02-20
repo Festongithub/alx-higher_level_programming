80315efa9ce013ecf5d4530f07acfc2781dabea5
Festongithub <fkemboik@gmail.com>
2024-02-20 02:14:59 -0800

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@ def read_file(filename=""):
     """
     reads a text file 
     """
-    with open(filename, encoding='UTF8') as f:
+    with open(filename,"r",  encoding='UTF8') as f:
         print(f.read(), end="")
