83b206f296a56aac42ae002cff63c2935c214357
Festongithub <fkemboik@gmail.com>
2024-02-21 00:01:32 -0800

    Inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,60 +1,24 @@
 #!/usr/bin/python3
 
 """
-An empty class
+This  module inherits from the 9-rectangle.py
 """
 
-
-class BaseGeometry:
-
-    """
-    Class called BaseGeometry
-    """
-
-    def area(self):
-        raise Exception("area is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-
-        # validate with integer_validator
-
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width} / {self.__height}"
-
-    def __repr__(self):
-        return f"[Rectangle] {self.__width} / {self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    def __init__(self, size):
-        self.__size = size
 
-    # validate with integer_validator
-    self.integer_validator("size", self.__size)
+    def __init__(self, size):
+        """
+        Instance of the square
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
 
     def area(self):
+        """
+        area implementation
+        """
         return self.__size * self.__size
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width} / {self.__height}"
-
-    def __repr__(self):
-        return f"[Rectangle] {self.__width} / {self.__height}"
-
