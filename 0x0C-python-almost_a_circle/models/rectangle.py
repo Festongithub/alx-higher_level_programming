ae6e09a7fde528e03958135d97f21a1f16eb7386
Festongithub <fkemboik@gmail.com>
2024-03-05 01:09:28 -0800

    Base Class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 
-"""
-This module inherits from the Base class
-"""
+""" Define the class rectangle """
+
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """
-    Inherits from the Base class
-    """
+    """The class inherits from the class Base """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Instance of the Rectangle
-        """
+        """The class instantiation """
+
         super().__init__(id)
         self.width = width
         self.height = height
@@ -24,73 +20,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """
-        Returns the value of width
-        """
+        """Returns the value of the width """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        Property setter that
-        Modifies the value of the width
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """Property setter for the width """
         self.__width = value
 
     @property
     def height(self):
-        """
-        Returns value of height
-        """
+        """Returns the value of the height """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Modifies the value of the width
-        """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height  must be > 0")
+        """Property setter for the height """
         self.__height = value
 
     @property
     def x(self):
-        """
-        Returns value of x
-        """
-        return self.__x
+        """Returns the value of the x  """
+        return self.__y
 
     @x.setter
     def x(self, value):
-        """
-        Modifies the value of x
-        """
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x  must be >=  0")
+        """Property setter for the x """
         self.__x = value
 
     @property
     def y(self):
-        """
-        Returns value of y
-        """
+        """Returns the value of the y """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """
-        Modifies the value of y
-        """
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y  must be >= 0")
+        """Property setter for the y """
         self.__y = value
