882c539ac75897e561a4a2281a9d40e6aa8eb57b
Festongithub <fkemboik@gmail.com>
2024-04-26 02:47:49 -0700

    cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""The module performs some sql injection on the database"""
+import MySQLdb
+from sys import argv
+# The code cannot be executed
+if __name__ == '__main__':
+    # connect to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    # run cursor on the database
+    cur = db.cursor()
+    # select from the database
+    cur.execute("SELECT * FROM cities")
+    # get all states
+    cities = cur.fetchall()
+    # print all states
+    for i in cities:
+        print(i)
+        # clean up
+        cur.close()
