a0d2dea20d80d3e6af8b139d6f6533ce86760220
Festongithub <fkemboik@gmail.com>
2024-04-26 05:01:48 -0700

    Object Relational mapper

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that prints the first State object from the database
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker()
+    session = Session(bind=engine)
+
+    Base.metadata.create_all(engine)
+
+    update_s = session.query(State).filter_by(id='2').first()
+    update_s.name = "New Mexico"
+    session.commit()
+    session.close()
