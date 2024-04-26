fe2d69a8c97e0dc2307b5562ab4dd0a0895b6c2a
Festongithub <fkemboik@gmail.com>
2024-04-26 02:22:05 -0700

    selcect states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""The module lists all the states"""
+import MySQLdb
+from sys import argv
+# The code cannot be executed
+if __name__ == '__main__':
+    # connect to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    # run cursor on the database
+    cur = db.cursor()
+    # select from the database
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    # get all states
+    states = cur.fetchall()
+    # print all states
+    for i in states:
+        print(i)
+        # clean up
+        cur.close()
