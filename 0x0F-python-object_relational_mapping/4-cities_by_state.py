e0de622edca5cc42be163345a16bf2090f038cca
Festongithub <fkemboik@gmail.com>
2024-04-26 02:55:45 -0700

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,9 @@ if __name__ == '__main__':
     # run cursor on the database
     cur = db.cursor()
     # select from the database
-    cur.execute("SELECT * FROM cities")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ASC")
     # get all states
     cities = cur.fetchall()
     # print all states
