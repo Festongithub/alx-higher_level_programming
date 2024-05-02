30eda211aba2278d34a95aedfa80aced2efc8ee4
Festongithub <fkemboik@gmail.com>
2024-05-02 03:07:08 -0700

    Object Relational mapper

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
+"""
+Class definition of a City
+"""
 
-""" creates a state model for the base"""
-from sqlalchemy import Column, String,  Integer, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
+
 
 class City(Base):
     """
-    class attributes
+    City class that inherits from Base
+
+    Attributes:
+        id: Id city
+        name: Name of the city
+        state_id: State id
     """
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
