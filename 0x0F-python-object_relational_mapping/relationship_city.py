247096ee57a57e085e13cb08fe0e05eb233ede64
Festongithub <fkemboik@gmail.com>
2024-05-02 03:16:04 -0700

    Object Relational mapper

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, text, ForeignKey
+"""
+Class definition of a City
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey, text
 from relationship_state import Base
-"""
-    Module that performs creates a States class based off of Base.
-"""
 
 
 class City(Base):
     """
-        The ``City`` class which inherits from ``Base`` class.
+    City class that inherits from Base
+
+    Attributes:
+        id: Id city
+        name: Name of the city
+        state_id: State id
     """
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
