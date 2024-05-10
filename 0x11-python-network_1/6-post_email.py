cde34661755530863a01a4156fecdb6845647747
Festongithub <fkemboik@gmail.com>
2024-05-10 04:28:43 -0700

    Python network

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
             "email": email
             }
 
-    response = requests.post(url,data=email_data)
+    response = requests.post(url, data=email_data)
     print(response.text)
