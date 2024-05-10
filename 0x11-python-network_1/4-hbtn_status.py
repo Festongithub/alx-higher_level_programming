bab05aea84d17c1ed57959c98291acb60d236d20
Festongithub <fkemboik@gmail.com>
2024-05-10 03:51:30 -0700

    Python network

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+ Python script that fetches https://alx-intranet.hbtn.io/status
+ """
+
+import requests
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    content = r.text
+    print("Body response")
+    print("-type: {}".format(type(content)))
+    print("-content: {}".format(content))
