a86d11ed64a23d4a46fe08b594ae1efa5ced7b46
Festongithub <fkemboik@gmail.com>
2024-05-10 04:19:37 -0700

    Python network

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    ulr = sys.argv[1]
+    email = sys.argv[2]
+    email_data = {
+            "email": email
+            }
+
+    response = requesta.post(url, email_data)
+    print(response.text)
