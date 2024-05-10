36ccf9866a230d32165c5ad7b5c34c8b2d5e1854
Festongithub <fkemboik@gmail.com>
2024-05-10 03:34:49 -0700

    Python network

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,11 +11,9 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     try:
-        # Send the request to the URL
         with urllib.request.urlopen(url) as response:
             body = response.read().decode('utf-8')
             print(body)
 
 except urllib.error.HTTPError as e:
-    # Print the error code
     print("Error code:{}".format(e.code))
