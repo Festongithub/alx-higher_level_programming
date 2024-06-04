cfbc86a98f47e795411dde0cd00181e66d499d06
Festongithub <fkemboik@gmail.com>
2024-06-04 04:38:26 -0700

    web scrapping

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usrbin/node
+
+const request = require('request');
+
+const end_Point = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request.get(end_Point, function (err, response, body) {
+	if (err) {
+		throw err;
+	} else if (response.statusCode === 200) {
+		const res = JSON.parse(body).res;
+		const list = [];
+		res.forEach(res => {
+			list.push(new Promise(( resolve, reject) => {
+				request.get(res, function (err, response, body) {
+					if (err) {
+						reject(err);
+					}else if (response.statusCode === 200) {
+						resolve(JSON.parse(body).name);
+					}
+				});
+			}));
+		});
+		Promise.all(list).then(names => {
+			names.forEach(name => console.log(name));
+		});
+	}
+});
