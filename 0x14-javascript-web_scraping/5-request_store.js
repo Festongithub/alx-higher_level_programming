776090662ae83209629691218dc0c9748e64c0ff
Festongithub <fkemboik@gmail.com>
2024-06-04 05:13:36 -0700

    web scrapping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 
 // The first argument is the URL to request
 const baseURL = process.argv[2];
-//The second argument the file path to store the body response
+// The second argument the file path to store the body response
 const bodyResp = process.argv[3];
 request(baseURL, (error, response, body) => {
-	if (error == null) {
-		fs.writeFileSync(bodyResp, body);
-	}
+  if (error == null) {
+    fs.writeFileSync(bodyResp, body);
+  }
 });
