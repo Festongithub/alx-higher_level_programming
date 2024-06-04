a7782010679346a13e2e27ba5c8d8d9f91bb3640
Festongithub <fkemboik@gmail.com>
2024-06-04 04:56:07 -0700

    web scrapping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,9 +6,8 @@ const process = require('process');
 // get the url
 const url = process.argv[2];
 
-request(url, function(error,response) {
-	if (error == null)
-	{
-		console.log(`code: ${response.statusCode}`);
-	}
+request(url, function (error, response) {
+  if (error == null) {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
