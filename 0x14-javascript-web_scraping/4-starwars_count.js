1acaa918d296ca664ee81c33fe93ac337c607d06
Festongithub <fkemboik@gmail.com>
2024-06-04 05:01:51 -0700

    web scrapping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,14 +5,14 @@ const request = require('request');
 // URL args
 const url = process.argv[2];
 
-request(url, (error, response,body) => {
-	if (error) {
-		console.log(error);
-	}else if (body) {
-		const json = JSON.parse(body);
-		const charFilms = json.result.filter(
-			y = y.characters.find(x => x.match(/\/people\/18\/?$/))
-		);
-		console.log(charFilms.length);
-	}
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (body) {
+    const json = JSON.parse(body);
+    const charFilms = json.result.filter(
+      y => y.characters.find(x => x.match(/\/people\/18\/?$/))
+    );
+    console.log(charFilms.length);
+  }
 });
